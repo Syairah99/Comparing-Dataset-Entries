1a39ac2e7b9c549694504677a3032633dc69870e
NUR SYAIRAH BINTI MOHD IRWAN <nursyairahirwan@gmail.com>
2023-01-13 16:46:51 +0800

    Add files via upload
    Simple examples on comparisons

---
+++ Comparing_Dataset_Entries.py
@@ -0,0 +1,31 @@
+# %%
+
+# Storing data in variables
+
+# Student 1
+id_1 = "#4"
+average_grade_1 = "A"
+test_score_1 = 90
+
+# Student 2
+id_2 = "#5"
+average_grade_2 = "A"
+test_score_2 = 70
+
+# Comparing data
+
+# Not equal (!=) comparison
+no_duplicates = id_1 != id_2
+print("No duplicate entries:")
+print(no_duplicates)
+
+# Equal (==) comparison
+same_average = average_grade_1 == average_grade_2
+print("Same average grade:")
+print(same_average)
+
+# More/less than (>/<) comparison
+higher_score = test_score_1 > test_score_2
+print("id_1 has a higher score:")
+print(higher_score)
+# %%
